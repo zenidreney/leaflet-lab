c13514112eeb9b1f29dff73763dbcebf1465645f
Deniz <auo@tuta.io>
2025-11-27 16:40:03 +0100

    added try and catch to getCoordinates.

--- src/utils/getCoordinates.ts
+++ src/utils/getCoordinates.ts
@@ -1,15 +1,25 @@
 export async function getCoordinates(location: string) {
-    const res = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${location}`, {
-        headers: { 'User-Agent': 'van-trip-explorer-student-project/1.0' }
-    })
+    try {
+        const res = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${location}`, {
+            headers: { 'User-Agent': 'van-trip-explorer-student-project/1.0' }
+        })
 
-    const data = await res.json()
-    //console.log(typeof data[0].name, typeof data[0].lat, typeof data[0].lon)
+        if (!res.ok) {
+            console.log(res.status)
+            throw new Error(`Request Error Code: ${res.status} `)
+        }
 
-    return {
-        name: data[0].name,
-        latitude: data[0].lat,
-        longitude: data[0].lon
+        const data = await res.json()
+        //console.log(typeof data[0].name, typeof data[0].lat, typeof data[0].lon)
+
+        return {
+            name: data[0].name,
+            latitude: data[0].lat,
+            longitude: data[0].lon
+        }
+    } catch (error) {
+        console.log(error)
+        throw error
     }
-    
+
 }
